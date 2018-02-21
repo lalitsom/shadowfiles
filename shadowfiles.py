56c3d58a417a686ebc7b45301a2cfd833f28f340
lalitsom <lalitsom27@gmail.com>
2018-02-21 19:33:43 +0530

    The first commit of actual code

---
+++ shadowfiles.py
@@ -0,0 +1,78 @@
+import urllib
+from HTMLParser import HTMLParser
+import os
+
+print '\n Welcome to Shadow Files :: \n'
+print ':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n'
+print  'Need help: File an issue  on https://github.com/lalitsom/shadowfiles/issues'
+print ':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n'
+print 'copy paste the link of an online directory \n'
+rooturl = raw_input()
+print 'Enter a folder name (eg. downloads)\n'
+rootdir = raw_input()
+
+def createdir(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
+# append / if not present
+if rooturl[-1] != '/':
+    rooturl += '/'
+
+
+# append / if not present
+if rootdir[-1] != '/':
+    rootdir += '/'
+
+# folders to traverse... BFS QUEUE
+folders = []
+
+
+template = open('template.xspf').read()
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global folders
+        global rooturl
+        global rootdir
+        if tag=='a':
+            for ats in attrs:
+                if ats[0] == 'href':
+                    lnkname = ats[1]
+                    if lnkname[-1] == '/':
+                        folders.append([rooturl+lnkname,rootdir+lnkname])
+                        createdir(rootdir+lnkname)
+                    else:
+                        print rooturl+lnkname +" ---> "+rootdir+lnkname
+                        outfile=open('./'+rootdir+lnkname+".xspf", 'w+')
+                        outfile.write(template.replace("##sourcelink##",rooturl+lnkname))
+                        outfile.close()
+
+    def handle_endtag(self, tag):
+        tmp = 6
+
+    def handle_data(self, data):
+        tmp = 6
+
+
+
+def traverse(link, _dir):
+    global rootdir
+    global rooturl
+    rootdir = _dir
+    rooturl = link
+    parser = MyHTMLParser()
+    createdir(_dir)
+    response = urllib.urlopen(link)
+    htmldata = response.read()
+    parser.feed(htmldata)
+    parser.close()
+
+traverse(rooturl,rootdir)
+
+while folders:
+    link,folder = folders.pop()
+    if '../' in link:
+        continue
+    traverse(link,folder)
